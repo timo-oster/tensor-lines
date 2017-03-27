b36cba3ca54ab0e7d657bb6ea348489ed3f77c38
Timo Oster <timo.oster@ovgu.de>
2017-03-27 14:58:27 +0200

    Switch to parallelization with OpenMP and only compute each face once

--- cpp/src/vtkParallelEigenvectors.cc
+++ cpp/src/vtkParallelEigenvectors.cc
@@ -6,16 +6,14 @@
 
 #include <vtkPolyData.h>
 #include <vtkUnstructuredGrid.h>
+#include <vtkDataSet.h>
 #include <vtkDataSetAttributes.h>
 #include <vtkSmartPointer.h>
 #include <vtkCellIterator.h>
-#include <vtkTetra.h>
-#include <vtkGenericCell.h>
-#include <vtkCellArray.h>
-#include <vtkMergePoints.h>
+#include <vtkPoints.h>
 #include <vtkDoubleArray.h>
+#include <vtkDataArray.h>
 #include <vtkPointData.h>
-#include <vtkCellData.h>
 #include <vtkIdList.h>
 
 #include <vtkCommand.h>
@@ -24,15 +22,157 @@
 #include <vtkObjectFactory.h>
 #include <vtkStreamingDemandDrivenPipeline.h>
 
+#include <algorithm>
 #include <map>
 #include <vector>
 #include <list>
+#include <array>
 #include <unordered_set>
+#include <unordered_map>
 #include <iostream>
-#include <future>
 #include <chrono>
 
-#include <ctpl_stl.h>
+namespace
+{
+
+// vtk Tensors are stored in row major order by convention
+using Mat3d = Eigen::Matrix<double, 3, 3, Eigen::RowMajor, 3, 3>;
+using Mat3dm = Eigen::Map<Mat3d>;
+using Vec3d = peigv::Vec3d;
+using Vec3dm = Eigen::Map<Vec3d>;
+
+struct TriFace
+{
+    std::array<vtkIdType, 3> points;
+    friend bool operator==(const TriFace& t1, const TriFace& t2)
+    {
+        return t1.points == t2.points;
+    }
+};
+
+struct FaceHash
+{
+    std::size_t offset;
+
+    FaceHash(std::size_t offset): offset(offset)
+    {}
+
+    std::size_t operator()(const TriFace& face) const
+    {
+        auto pts = face.points;
+        std::sort(std::begin(pts), std::end(pts));
+        return std::hash<std::size_t>{}(
+                    std::size_t(pts[0])
+                    + offset * std::size_t(pts[1])
+                    + offset*offset * std::size_t(pts[2]));
+    }
+};
+
+struct FaceData
+{
+    TriFace face;
+    std::array<Mat3d, 3> s_tensors;
+    std::array<Mat3d, 3> t_tensors;
+};
+
+using FaceMap = std::unordered_map<TriFace, std::vector<vtkIdType>, FaceHash>;
+
+FaceMap buildFaceMap(vtkDataSet* dataset)
+{
+    auto face_map = FaceMap(std::size_t(dataset->GetNumberOfCells()*4),
+                            FaceHash(std::size_t(dataset->GetNumberOfPoints())));
+
+    auto add_face = [&](vtkIdList* point_ids, vtkIdType cell_id,
+                        vtkIdType i1, vtkIdType i2, vtkIdType i3)
+    {
+        auto tri = TriFace{{point_ids->GetId(i1),
+                            point_ids->GetId(i2),
+                            point_ids->GetId(i3)}};
+
+        auto f = face_map.find(tri);
+        if(f != std::end(face_map))
+        {
+            f->second.push_back(cell_id);
+        }
+        else
+        {
+            face_map[tri] = std::vector<vtkIdType>{cell_id};
+        }
+    };
+
+    // Collect unique faces and remember participating cells
+    auto it = vtkSmartPointer<vtkCellIterator>(dataset->NewCellIterator());
+    for(it->InitTraversal(); !it->IsDoneWithTraversal(); it->GoToNextCell())
+    {
+        if(it->GetCellType() != VTK_TETRA) continue;
+
+        auto* point_ids = it->GetPointIds();
+        auto cid = it->GetCellId();
+
+        add_face(point_ids, cid, 0, 1, 2);
+        add_face(point_ids, cid, 1, 3, 2);
+        add_face(point_ids, cid, 0, 3, 1);
+        add_face(point_ids, cid, 0, 2, 3);
+    }
+    return face_map;
+}
+
+std::vector<peigv::PointList> computePEVPoints(const std::vector<TriFace>& faces,
+                                               vtkPoints* points,
+                                               vtkDataArray* array1,
+                                               vtkDataArray* array2,
+                                               vtkAlgorithm* progress_alg,
+                                               double spatial_epsilon,
+                                               double direction_epsilon,
+                                               double cluster_epsilon,
+                                               double parallelity_epsilon)
+{
+    const auto step = 1./faces.size();
+    progress_alg->UpdateProgress(0);
+    auto results = std::vector<peigv::PointList>(faces.size());
+    #pragma omp parallel for
+    for(auto i = std::size_t{0}; i < faces.size(); ++i)
+    {
+        auto face = faces[i];
+
+        auto p1 = Vec3d{};
+        points->GetPoint(face.points[0], p1.data());
+        auto p2 = Vec3d{};
+        points->GetPoint(face.points[1], p2.data());
+        auto p3 = Vec3d{};
+        points->GetPoint(face.points[2], p3.data());
+
+        auto s1 = Mat3d{};
+        array1->GetTuple(face.points[0], s1.data());
+        auto s2 = Mat3d{};
+        array1->GetTuple(face.points[1], s2.data());
+        auto s3 = Mat3d{};
+        array1->GetTuple(face.points[2], s3.data());
+
+        auto t1 = Mat3d{};
+        array2->GetTuple(face.points[0], t1.data());
+        auto t2 = Mat3d{};
+        array2->GetTuple(face.points[1], t2.data());
+        auto t3 = Mat3d{};
+        array2->GetTuple(face.points[2], t3.data());
+
+        auto points = peigv::findParallelEigenvectors(s1, s2, s3,
+                                                      t1, t2, t3,
+                                                      p1, p2, p3,
+                                                      spatial_epsilon,
+                                                      direction_epsilon,
+                                                      cluster_epsilon,
+                                                      parallelity_epsilon);
+        results[i] = points;
+        #pragma omp critical (progress)
+        {
+            progress_alg->UpdateProgress(progress_alg->GetProgress() + step);
+        }
+    }
+    return results;
+}
+
+}
 
 vtkStandardNewMacro(vtkParallelEigenvectors);
 
@@ -174,18 +314,12 @@ int vtkParallelEigenvectors::RequestData(
     vtkInformationVector **inputVector,
     vtkInformationVector* outputVector )
 {
-    using namespace peigv;
-
+    using peigv::range;
     using namespace std::chrono;
     using hours = duration<double, std::chrono::hours::period>;
     using minutes = duration<double, std::chrono::minutes::period>;
     using seconds = duration<double, std::chrono::seconds::period>;
     using milliseconds = duration<double, std::chrono::milliseconds::period>;
-
-    // vtk Tensors are stored in row major order by convention
-    using Mat3d = Eigen::Matrix<double, 3, 3, Eigen::RowMajor, 3, 3>;
-    using Mat3dm = Eigen::Map<Mat3d>;
-    using Vec3dm = Eigen::Map<peigv::Vec3d>;
 
     auto* outInfo = outputVector->GetInformationObject(0);
     auto* output = vtkPolyData::SafeDownCast(
@@ -244,140 +378,39 @@ int vtkParallelEigenvectors::RequestData(
     imag2->SetName("Imaginary 2");
     output->GetPointData()->AddArray(imag2);
 
-    // Structure for mapping cell IDs to parallel eigenvector points
-    auto cell_map = std::map<vtkIdType, vtkSmartPointer<vtkIdList>>{};
+    // map faces to cell ids
+    auto face_map = buildFaceMap(input);
 
-    ctpl::thread_pool tpool(4);
-    auto futures = std::list<
-                        std::pair<vtkIdType,
-                                  std::future<peigv::PointList>
-                        >
-                    >{};
-
-    auto find_pev_points = [](int /*tid*/,
-                              const Mat3d& s1, const Mat3d& s2, const Mat3d& s3,
-                              const Mat3d& t1, const Mat3d& t2, const Mat3d& t3,
-                              const Vec3d& p1, const Vec3d& p2, const Vec3d& p3,
-                              double spatial_epsilon, double direction_epsilon,
-                              double cluster_epsilon, double parallelity_epsilon)
+    // Copy faces to array for parallel looping
+    auto faces = std::vector<TriFace>{};
+    faces.reserve(face_map.size());
+    for(const auto& f: face_map)
     {
-        return peigv::findParallelEigenvectors(s1, s2, s3, t1, t2, t3, p1, p2, p3,
-                                               spatial_epsilon, direction_epsilon,
-                                               cluster_epsilon, parallelity_epsilon);
-    };
+        faces.push_back(f.first);
+    }
 
     auto start = high_resolution_clock::now();
 
-    auto progress = 0.;
-    auto step = 1. / double(input->GetNumberOfCells()*4);
+    auto fresults = computePEVPoints(faces,
+                                     input->GetPoints(),
+                                     array1,
+                                     array2,
+                                     this,
+                                     this->GetSpatialEpsilon(),
+                                     this->GetDirectionEpsilon(),
+                                     this->GetClusterEpsilon(),
+                                     this->GetParallelityEpsilon());
 
-    auto it = vtkSmartPointer<vtkCellIterator>(input->NewCellIterator());
-    for(it->InitTraversal(); !it->IsDoneWithTraversal(); it->GoToNextCell())
+    auto end_pointsearch = high_resolution_clock::now();
+
+    // map cell IDs to parallel eigenvector points found on their faces
+    auto cell_map = std::map<vtkIdType, vtkSmartPointer<vtkIdList>>{};
+
+    for(auto i: range(faces.size()))
     {
-        if(it->GetCellType() != VTK_TETRA)
-        {
-            continue;
-        }
-
-        auto* cell_points = it->GetPoints();
-        auto* point_ids = it->GetPointIds();
-        assert(it->GetNumberOfPoints() == 4);
-
-        // Add parallel eigenvector points on the face with the given indices
-        // to the output dataset
-        auto add_peigv_points = [&](int i1, int i2, int i3)
-        {
-            auto p1 = Vec3d{Vec3dm{cell_points->GetPoint(i1)}};
-            auto p2 = Vec3d{Vec3dm{cell_points->GetPoint(i2)}};
-            auto p3 = Vec3d{Vec3dm{cell_points->GetPoint(i3)}};
-
-            auto s1 = Mat3d{Mat3dm{array1->GetTuple(point_ids->GetId(i1))}};
-            auto s2 = Mat3d{Mat3dm{array1->GetTuple(point_ids->GetId(i2))}};
-            auto s3 = Mat3d{Mat3dm{array1->GetTuple(point_ids->GetId(i3))}};
-
-            auto t1 = Mat3d{Mat3dm{array2->GetTuple(point_ids->GetId(i1))}};
-            auto t2 = Mat3d{Mat3dm{array2->GetTuple(point_ids->GetId(i2))}};
-            auto t3 = Mat3d{Mat3dm{array2->GetTuple(point_ids->GetId(i3))}};
-
-            // Only process faces where all tensors are above the threshold
-            if(s1.norm() < this->GetMinTensorNorm()
-               || s2.norm() < this->GetMinTensorNorm()
-               || s3.norm() < this->GetMinTensorNorm()
-               || t1.norm() < this->GetMinTensorNorm()
-               || t2.norm() < this->GetMinTensorNorm()
-               || t3.norm() < this->GetMinTensorNorm())
-            {
-                progress += step;
-                this->UpdateProgress(progress);
-                return;
-            }
-
-            auto handle = tpool.push(
-                    find_pev_points,
-                    s1, s2, s3, t1, t2, t3, p1, p2, p3,
-                    this->GetSpatialEpsilon(), this->GetDirectionEpsilon(),
-                    this->GetClusterEpsilon(),  this->GetParallelityEpsilon());
-
-            futures.push_back(std::make_pair(it->GetCellId(), std::move(handle)));
-        };
-        add_peigv_points(0, 1, 2);
-        add_peigv_points(1, 3, 2);
-        add_peigv_points(0, 3, 1);
-        add_peigv_points(0, 2, 3);
-
-        // Wait for some completed tasks here so the waiting list doesn't get
-        // too long and clogs up memory
-        while(futures.size() > 256)
-        {
-            for(auto it = std::begin(futures); it != std::end(futures); ++it)
-            {
-                auto& v = *it;
-                if(!v.second.valid())
-                {
-                    std::cout << "Got an invalid future for cell " << v.first << std::endl;
-                    continue;
-                }
-                if(v.second.wait_for(std::chrono::milliseconds{1}) != std::future_status::ready)
-                {
-                    continue;
-                }
-                auto cid = v.first;
-                auto points = v.second.get();
-                for(const auto& p: points)
-                {
-                    auto pid = output->GetPoints()->InsertNextPoint(p.pos.data());
-                    eig_rank1->InsertValue(pid, double(p.s_rank));
-                    eig_rank2->InsertValue(pid, double(p.t_rank));
-                    eival1->InsertValue(pid, p.s_eival);
-                    eival2->InsertValue(pid, p.t_eival);
-                    eivec->InsertTuple(pid, p.eivec.data());
-                    imag1->InsertValue(pid, p.s_has_imaginary ? 1. : 0.);
-                    imag2->InsertValue(pid, p.t_has_imaginary ? 1. : 0.);
-                    if(!cell_map[cid].Get())
-                    {
-                        cell_map[cid] = vtkSmartPointer<vtkIdList>::New();
-                    }
-                    cell_map[cid]->InsertNextId(pid);
-                }
-                progress += step;
-                this->UpdateProgress(progress);
-                it = futures.erase(it);
-                --it;
-            }
-        }
-    }
-
-    // Wait for the remaining tasks to complete
-    for(auto& v: futures)
-    {
-        if(!v.second.valid())
-        {
-            std::cout << "Got an invalid future for cell " << v.first << std::endl;
-            continue;
-        }
-        auto cid = v.first;
-        auto points = v.second.get();
-        for(const auto& p: points)
+        auto pev_points = fresults[i];
+        auto cids = face_map[faces[i]];
+        for(const auto& p: pev_points)
         {
             auto pid = output->GetPoints()->InsertNextPoint(p.pos.data());
             eig_rank1->InsertValue(pid, double(p.s_rank));
@@ -387,17 +420,18 @@ int vtkParallelEigenvectors::RequestData(
             eivec->InsertTuple(pid, p.eivec.data());
             imag1->InsertValue(pid, p.s_has_imaginary ? 1. : 0.);
             imag2->InsertValue(pid, p.t_has_imaginary ? 1. : 0.);
-            if(!cell_map[cid].Get())
-            {
-                cell_map[cid] = vtkSmartPointer<vtkIdList>::New();
-            }
-            cell_map[cid]->InsertNextId(pid);
-        }
-        progress += step;
-        this->UpdateProgress(progress);
-    }
 
-    auto end_pointsearch = high_resolution_clock::now();
+            // Add pev point to all cells participating in this face
+            for(auto c: cids)
+            {
+                if(!cell_map[c].Get())
+                {
+                    cell_map[c] = vtkSmartPointer<vtkIdList>::New();
+                }
+                cell_map[c]->InsertNextId(pid);
+            }
+        }
+    }
 
     output->SetLines(vtkCellArray::New());
 
